96e171d4b0c7aee2f6b121a43dbd0108d4cd1046
gaurav <gauravp@zignuts.com>
2023-02-27 15:54:04 +0530

    Materialize Css Practice navbar and modal

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
     $('.modal').modal();
     $('.parallax').parallax();
+    $('.sidenav').sidenav();
 });
 
 
 function openModal() {
     var instance = M.Modal.getInstance($('#modal'));
+    instance.open();
+}
+
+function openNav(){
+    var instance = M.Sidenav.getInstance($('.sidenav'));
     instance.open();
 }
